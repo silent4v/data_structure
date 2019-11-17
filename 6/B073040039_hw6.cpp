2b59c1c532354344c8f8eb64e1c03ad7943541b6
daven3302 <daven3302@gmail.com>
2019-11-17 14:34:35 +0800

    final
    new method for tree :
    clear():
        clear nodes of tree
    debugged method for tree :
    del():
        successor check and delete successfully
    new global function :
    print():
        for output
    new variable:
    input:
        a list for input

--- 6/B073040039_hw6.cpp
+++ 6/B073040039_hw6.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <stack>
+#include <map>
+#include <list>
+/*
+    author:B073040039
+    date:2019/11/17
+    description:bst
+    ps: c++1x
+*/
 using namespace std;
 class node
 {
@@ -8,32 +16,13 @@ class node
         node *lch,*rch,*dad;
         node(int x) {value = x; dad = lch = rch = nullptr;}
         node() {value = 0; dad = lch = rch = nullptr;}
-        void info()
-        {
-            cout << "dad : ";
-            if(this->dad == nullptr)
-                cout << "nullptr" << endl;
-            else
-                cout << this->dad->value << endl;
-            cout << "lch : ";
-            if(this->lch == nullptr)
-                cout << "nullptr" << endl;
-            else
-                cout << this->lch->value << endl;
-            cout << "rch : ";
-            if(this->rch == nullptr)
-                cout << "nullptr" << endl;
-            else
-                cout << this->rch->value << endl;
-        }
 };
-
+map<int,node*>output;
 class tree
 {
     private:
         node *root;
     public:
-        void rootinfo() {root->info();}
         bool lr(node *child,node *father) {return (father->lch == child);}  //check child item of node,true for lch,false for rch
         const string state(node *x)
         {
@@ -46,10 +35,18 @@ class tree
         }
         void del(node* x)
         {
-            bool child = lr(x,x->dad);
+            bool child;
+            if(x != root)
+                child = lr(x,x->dad);
             string check = state(x);
             if(check == "leaf")
             {
+                if(x == root)
+                {
+                    delete x;
+                    root = nullptr;
+                    return;
+                }
                 x = x->dad;
                 if(child)
                 {
@@ -70,6 +67,15 @@ class tree
                     son_position = false;   //if son is rch,son_position is false
                 else 
                     son_position = true;
+                if(x == root)
+                {
+                    if(son_position)
+                        root = x->lch;
+                    else
+                        root = x->rch;
+                    delete x;
+                    return;
+                }
                 if(son_position)
                 {
                     x->lch->dad = x->dad;
@@ -79,12 +85,12 @@ class tree
                         x->dad->rch = x->lch;
                 }
                 else
-                {
+                {                    
                     x->rch->dad = x->dad;
                     if(child)
-                        x->dad->lch = x->lch;
+                        x->dad->lch = x->rch;
                     else
-                        x->dad->rch = x->lch;
+                        x->dad->rch = x->rch;
                 }
                 delete x;
             }
@@ -94,33 +100,34 @@ class tree
                 succesor = x->rch;
                 while(succesor->lch != nullptr)
                     succesor = succesor->lch;
-                if(state(succesor) == "leaf")
-                {
-                    
-                    succesor = succesor->dad;
-                    delete succesor->lch;
-                    succesor->lch = nullptr;
-                }
-                else 
+                x->value = succesor->value;
+                child = lr(succesor,succesor->dad);
+                if(state(succesor) != "leaf")   //if succesor has a child,it will be rch
                 {
                     succesor->rch->dad = succesor->dad;
-                    succesor->dad->lch = succesor->rch;
-                    delete succesor; 
+                    if(child)
+                        succesor->dad->lch = succesor->rch;
+                    else
+                        succesor->dad->rch = succesor->rch;
                 }
+                else
+                {
+                    if(child)
+                        succesor->dad->lch = nullptr;
+                    else
+                        succesor->dad->rch = nullptr;
+                }
+                output[x->value] = x;
+                delete succesor;
             }
             
         }
         bool empty() {return (root == nullptr);}
-        tree() {root = nullptr;}
+        tree() {root = nullptr;}    //constructor of tree
         void add(node* x) 
         {
-            if(empty())
-            {
-                root = x;
-                return;
-            }
-            bool exist = false;
-            bool position;
+            if(empty()){ root = x; return;}
+            bool position,exist = false;
             node* now = root;
             node* prev = nullptr;
             while(now != nullptr)
@@ -140,6 +147,7 @@ class tree
                 else
                 {
                     del(now);
+                    output.erase(x->value);
                     exist = true;
                     break;
                 }
@@ -156,21 +164,67 @@ class tree
             }
             return;
         }
+        void clear()
+        {
+            for(auto &&[x,y]:output)
+                delete y;
+            root = nullptr;
+        }
 };
+void print()    //for output
+{
+    cout << "node:\t";
+        for(auto &&[x,y] : output )
+            if(y != nullptr)
+                cout << x << "\t";
+        cout << "\nleft:\t";
+        for(auto &&[x,y] : output )
+        {
+            if(y != nullptr)
+            {
+                if(y->lch != nullptr)
+                    cout << y->lch->value << "\t";
+                else
+                    cout << "0\t";
+            }
+        }
+        cout << "\nright:\t";
+        for(auto &&[x,y] : output )
+        {
+            if(y != nullptr)
+            {
+                if(y->rch != nullptr)
+                    cout << y->rch->value << "\t";
+                else
+                    cout << "0\t";
+            }
+        }
+        cout << "\n" << endl;
+}
 int main()
 {
     int n;
     tree data;
+    list<int>input;
     node *space;
     while(cin >> n)
     {
-        space = new node(n);
-        data.add(space);
-        cout << n << endl;
-        space->info();
-        cout << endl;
-        cout << "root : " << endl;
-        data.rootinfo();
+        if(n == -1)
+        {
+            for(auto &&x : input)
+            {
+                space = new node(x);
+                output[x] = space;
+                data.add(space);
+            }
+            print();
+            data.clear();
+            output.clear();
+            input.clear();
+        }
+        else
+            input.push_back(n);
     }
+    data.clear();
     return 0;
 }
